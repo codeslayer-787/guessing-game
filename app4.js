2c0a418eba89ab7c68690a941092f4219da27ecf
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-14 16:43:45 -0600

    Fix Lines in Js

--- app4.js
+++ app4.js
@@ -4,14 +4,13 @@ var arrayOfAnswers =[];
 
 let numberOfGuesses = 6      //Bucket example
 for (let i = 0; i < numberOfGuesses; i++) {
-  var answer = prompt('Can you name some of my favorite bands?');
+  var answer = prompt('Can you name some of my favorite mainstream bands?');
   let newAnswer= answer.toLowerCase();
   if (favBands.includes(newAnswer)){
     alert('That\'s right!');
   }
- 
 }
 
-alert('Good work!  My favorite bands are Avenged Sevenfold, Rammstein, Tool, Mudvayne, Primus, and Jinjer');
+alert('Good work!  Some of my favorite bands are Korn, Rammstein, Tool, Mudvayne, Primus, and Jinjer');
 
 //console.log(pets.includes('cat')); <---Example on how to check if a value is contained within my array
