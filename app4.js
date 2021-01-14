617945cd3aa86aa31cd630894ef8eb65477dc80b
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-14 10:42:52 -0600

    app4 needs work

--- app4.js
+++ app4.js
@@ -1,17 +1,34 @@
-const favBands = ["avenged", "rammstein", "tool", "mudvayne", "primus", "slipknot"]
+const favBands = ["korn", "rammstein", "tool", "mudvayne", "primus", "jinjer"]
 
 var arrayOfAnswers =[];
-var answer = prompt('What are some of my favorite bands?');
+var answer = prompt('Can you name some of my favorite bands?');
+let numberOfGuesses = 6
 let newAnswer= answer.toLowerCase();
   for (let i = 0; i < favBands.length; i++) {
     if(favBands[i] === newAnswer){
-   alert ('Good stuff');
+   alert ('That\'s right!');
     }
     else 
-   alert ('Nein');
-   break;
+   alert ('Nein!  My favorite bands are Avenged Sevenfold, Rammstein, Tool, Mudvayne, Primus, and Jinjer');
 }
 console.log(newAnswer);
+
+
+// const favBands = ["avenged", "rammstein", "tool", "mudvayne", "primus", "jinjer"]
+
+// var answer = prompt('Can you name one of my favorite bands?');
+// let newAnswer= answer.toLowerCase();
+//   for (let i = 0; i < favBands.length; i++) {
+//     if(newAnswer === favBands[i]){
+//    alert ('That\'s right! My favorite bands are Avenged Sevenfold, Rammstein, Tool, Mudvayne, Primus, and Jinjer');
+//     }
+//     else 
+//    alert ('Nein!  My favorite bands are Avenged Sevenfold, Rammstein, Tool, Mudvayne, Primus, and Jinjer');
+//    break;
+// }
+// console.log(newAnswer);
+
+
 
 
 
